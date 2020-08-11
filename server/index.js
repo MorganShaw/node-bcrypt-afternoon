8c9fcdef5d89c5ac875478aeb6545f3123f69540
user-1 <user-1@MacBook-Pro.local>
2020-08-11 14:30:57 -0600

    step 1 done

--- server/index.js
+++ server/index.js
@@ -5,13 +5,33 @@ const session = require('express-session');
 
 const PORT = 4002;
 
+const {CONNECTION_STRING, SESSION_SECRET } = process.env
+
 const app = express();
 
 app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then(db => {
+    app.set('db', db);
+    console.log('Database is connected');
+});
+//Where is db defined? I know that app.set() takes in two parameters, and one is db, and .then() takes in db into the callback function - and I know we could call that whatever we want. But where is it referring to the database? Is it because it's massive - and the purpose of massive invoked is to connect the database with the server? So the .then() in this case just knows that whatever element is in that callback function is what we're going to use to refer to the database(just because it's massive)? It's just what massive returns as a promise? Could you use async await here?
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET
+    })
+)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 })
 
 
-// app.set('db', db)
